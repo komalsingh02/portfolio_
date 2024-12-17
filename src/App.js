cfc3d24cff78e099a55cca1651e0419e765dc38e
komalsingh02 <komalsingh23748@gmail.com>
2024-12-17 16:19:46 +0530

    Layout

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Component/Nav";
+import "./index.css"; // or the path to your CSS file
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
